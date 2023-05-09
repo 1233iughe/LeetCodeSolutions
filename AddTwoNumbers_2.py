bdf23c70df302ffde61f6706ee43aaa65680dac3
1233iughe <None>
2023-05-09 15:01:31 -0500

    Extra commentary

--- AddTwoNumbers_2.py
+++ AddTwoNumbers_2.py
@@ -2,15 +2,24 @@
     Algorithm (brute force):
     1. Loop each element against all other elements 
     2. Check which will add up
-
+    Time: O(n²) 
+    Space: O(1)
+    
     Algorithm (improved)
     1. Loop trought each element 
     2. Find difference with target
     3. Check if difference is in the array
+    Time: O(n)
+    Space: O(n)
 
+    Best solution:
+    Use HashMap to implement a mapping val:index as you loop once trought the array.
+    While this happens check for the complement as in the improved algorithm but in the map itself. Its guarantee 
+    you will have the 1st element in the HashMap when you get to the second element
     Corner cases -> current element is equal to the difference
+    
     '''
-#Using difference mapping
+#Using difference mapping (HashMap using a list)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         copy_nums = nums.copy() 
