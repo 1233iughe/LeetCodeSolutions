2b8d8b632727e27ac8323e628b2de3d9e4d9412b
1233iughe <None>
2023-05-09 14:16:26 -0500

    Extra commentary on solution.

--- ContainsDuplicate_217.py
+++ ContainsDuplicate_217.py
@@ -1,3 +1,49 @@
 '''
 This problem is supposed to be solved using arrays, but a great number of solutions used sets or hash tables 
 '''
+#Strict array solution (times out)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        unique = []
+        n = len(nums)
+        for i in range(n // 2):
+            #print(i, unique, )
+            if nums[i] not in unique:
+                unique.append(nums[i])
+            else:
+                return True
+            if nums[n-i-1] not in unique:
+                unique.append(nums[n - i - 1])
+                continue  
+            else:
+                return True
+            
+        return False
+
+#Set solution O(n) both space and time
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)
+            
+#Hash table (dictionary) solution O(n) both space and time
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        unique = {}
+        n = len(nums)
+        if n  % 2 != 0: unique[str(nums[n // 2])] = nums[n//2]
+        for i in range(n // 2):
+            
+            a = str(nums[i])
+            b = str(nums[n-i-1])
+            #print(i, unique, a, b)
+            if a in unique:
+                return True
+            unique[a] = nums[i]
+            if b in unique:
+                return True 
+            unique[b] = nums[n-i-1] 
+            
+            
+        return False
---
+++ ValidAnagram_242.py
@@ -0,0 +1,25 @@
+'''
+ Possible solutions
+1. HashMap -> use it to create a map of letters and counts of those letters per string if they are the same then they are anagrams
+Time complexity O(s+t) Space complexity O(s+t) n =s+t
+2. Sorting -> Sort both strings and if they are equal the they are anagrams
+Time complexity O(n*logn) Space complexity O(1) if optimized
+
+Mine was HashMap with extra steps 
+Time O(2*(s+t)) Space O(2*(s+t)) aprox due to sets (it is overstimated)
+ '''       
+    
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_set = set(s)
+        t_set = set(t)
+        if s_set == t_set:
+            s_hash = {letter:s.count(letter) for letter in s_set}
+            t_hash = {letter:t.count(letter) for letter in t_set}
+            #print(s_hash, t_hash)
+            for key in s_hash.keys():
+                #print(s_hash[key])
+                if s_hash[key] != t_hash[key]:
+                    return False
+            return True
+        return False
