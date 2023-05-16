4ae068210d0c34ee50c991d7a8dc19cf652241ad
1233iughe <None>
2023-05-15 21:31:03 -0500

    Easy problem

--- LongestConsecutiveSequence_128.py
+++ LongestConsecutiveSequence_128.py
@@ -1,21 +1,26 @@
+'''
+Algorithm (heap):
+1. Create set of the list given
+2. Use min/max heap to check if every next -prev number is equal to 1
+3. Add 1 to counter and proceed or simply add counter to sequence and set counter to 1
+4. Return mac element in sequence
+Time: O(n)
+Space: O(n)
+
+Algorithm (set)
+1. Create set of the list given
+2. loop throught the set
+3. if the previous element is in there (n-1) it will continue the sequence
+4. If ended compare to previous biggest sequence
+Time: O(n)
+Space: O(n)
+
+'''
+#Heap
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         import heapq
-        '''
-        Possible to use heap
 
-        Consequtive means a diference of 1
-        [0,3,7,2,5,8,4,6,0,1]
-        1. Create the array with remainders of a[i]-/+1
-        [0,3,7,2,5,8,4,6,0,1] 0
-        [-1,2,6,1,5,7,3,5,-1,0] -1
-        [1,4,8,3,6,9,5,7,1,2] +1
-        2. Create a HashMap on Sets 
-        3. For each number check if it or one of its differences is already on a set
-            3.1 If not create a new entry key(the value):set and add it
-            3.2 If it is on set dont add it
-            3.3 
-        '''
         if len(nums) <= 1:
             return len(nums)
         nums = list(set(nums))
@@ -44,3 +49,25 @@ class Solution:
             return 1
         
         return max(sequences)
+
+#No heap by chappy1
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        #Setting a variable to keep track of the longest sequence
+        longest = 0
+        #Setting set to keep track of unique elements
+        num_set = set(nums)
+
+        #Looping through each individual element
+        for n in num_set:
+            #Checking if the previous element in the sequence is in set
+            if (n-1) not in num_set:
+                #Mrking the start of a sequence
+                length = 1
+                #While the sequence continues the elements n+1 exist in set
+                while (n+length) in num_set:
+                    length += 1
+                #Save the biggest number between the previous longest and the current sequence
+                longest = max(longest, length)
+        #Returns the longest sequence
+        return longest
